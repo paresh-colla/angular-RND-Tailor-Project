c1728ecf9b36fe99e9243df3b89f404a63cebd7f
paresh chouhan <paresh442@gmail.com>
2024-09-22 02:50:08 +0530

    feat: employee get api add

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-customer',
@@ -9,5 +10,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './customer.component.scss'
 })
 export class CustomerComponent {
+  data: Object | undefined;
 
+
+  constructor(
+    private http: HttpClient
+  ) {
+    
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/api/employee')
+      .subscribe(response => {
+        this.data = response;
+        console.log(this.data)
+      });
+  }
 }
